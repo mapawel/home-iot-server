f6b8c4cd2fd7f76294a4b0feb1810f634e763dec
mapawel <pawelma@gmail.com>
2023-09-02 00:41:55 +0200

    write radio tests7

--- src/app.ts
+++ src/app.ts
@@ -27,32 +27,25 @@ class Server {
       PALevel: nrf24.RF24_PA_LOW,
       DataRate: nrf24.RF24_1MBPS,
     });
-    console.log('--rf24 start -> ', rf24);
-    const pipe = rf24.addReadPipe('0x65646f4e31', false);
-    console.log('--pipe -> ', pipe);
+    rf24.addReadPipe('0x65646f4e31', true);
 
     const data: Buffer = Buffer.from('Hello mother fucker!');
-    rf24.useWritePipe('0x72646f4e31', false);
+    rf24.useWritePipe('0x72646f4e31', true);
 
     const go = () => {
       console.log('hasFailure ? -> ', rf24.hasFailure());
 
       rf24.stopRead();
       rf24.write(data);
-      rf24.read(
-        function (data: [{ pipe: string; data: Buffer }], n: number) {
-          for (let i = 0; i <= n; i++) {
-            console.log(
-              `>>>>> ${n} iter: `,
-              `pipe: ${data[n - 1]?.pipe}`,
-              `DATA: ${data[n - 1]?.data}`,
-            );
-          }
-        },
-        function (isStopped: unknown, by_user: unknown, error_count: unknown) {
-          console.log('RADIO STOPPED! -> ', isStopped, by_user, error_count);
-        },
-      );
+      rf24.read(function (data: [{ pipe: string; data: Buffer }], n: number) {
+        for (let i = 0; i <= n; i++) {
+          console.log(
+            `>>>>> ${n} iter: `,
+            `pipe: ${data[n - 1]?.pipe}`,
+            `DATA: ${data[n - 1]?.data}`,
+          );
+        }
+      });
     };
     let i = 0;
 
