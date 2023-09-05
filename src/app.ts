4af2b8988d94c44fd864db77cfb453d33d354f4e
mapawel <pawelma@gmail.com>
2023-09-06 00:30:58 +0200

    radio node again 19

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ class Server {
     console.log('is radio begin -> ', isRadioBegin);
 
     rf24.config(nrfConfig);
-    const pipeNo = rf24.addReadPipe(paddedHexAddress);
+    // const pipeNo = rf24.addReadPipe(paddedHexAddress);
     const pipeNo2 = rf24.addReadPipe(paddedHexAddress2);
     console.log('is pipe created, no: -> ', pipeNo);
     console.log('is pipe created, no: -> ', pipeNo2);
