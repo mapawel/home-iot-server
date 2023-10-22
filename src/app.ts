9f93a06ee6400b56d8cb5c7bc17e4e233c3012f1
Pawel <62116742+mapawel@users.noreply.github.com>
2023-10-22 09:05:21 +0200

    Radio2 (#3)
    nRf24 radio different settings and tests

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Express } from 'express';
 import Debugger from './app-services/debugger/debugger.service';
+// @ts-ignore
+import * as nrf24 from 'nrf24';
 import ConfigBuilder from './config-builder/Config-builder';
 import AppRouter from './app-router';
 import { configType } from './config-builder/config.type';
@@ -18,25 +20,64 @@ class Server {
   private appRouter: AppRouter | undefined;
 
   public startRadioForTest() {
-    console.log('radio start');
+    const nrfConfig = {
+      PALevel: nrf24.RF24_PA_HIGH,
+      DataRate: nrf24.RF24_1MBPS,
+      Channel: 100,
+      CRCLength: nrf24.RF24_CRC_16,
+      retriesCount: 10,
+      AutoAck: true,
+    };
+    const address: number = 100;
 
-    const spiDev = '/dev/spidev0.0';
-    const cePin = 17; //RPI_BPLUS_GPIO_J8_15
-    const irqPin = null;
-    const channel = 0x4c; //90
+    const hexAddress: string = address.toString(16).toUpperCase();
+    const paddedHexAddress: string = `0x${hexAddress.padStart(10, '0')}`;
 
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const radio = require('nrf').connect(spiDev, cePin, irqPin);
-    radio
-      .channel(channel)
-      .dataRate('1Mbps')
-      .crcBytes(2)
-      .autoRetransmit({ count: 15, delay: 4000 });
-    radio.begin(function () {
-      const rx = radio.openPipe('rx', 0xf0f0f0f0e1),
-        tx = radio.openPipe('tx', 0xf0f0f0f0d2);
-      rx.pipe(tx); // echo back everything
-    });
+    const rf24 = new nrf24.nRF24(17, 0);
+    const isRadioBegin: boolean = rf24.begin();
+
+    console.log('is radio begin -> ', isRadioBegin);
+
+    rf24.config(nrfConfig);
+    const pipeNo = rf24.addReadPipe(paddedHexAddress);
+    console.log('is pipe created, no: -> ', pipeNo);
+
+    console.log('has radio failure -> ', rf24.hasFailure());
+
+    rf24.read(
+      function (data: [{ pipe: string; data: Buffer }], frames: number) {
+        for (let i = 1; i <= frames; i++) {
+          console.log(
+            `>>> all frames: ${frames}.`,
+            `Frame no ${frames}: `,
+            `pipe: ${data[frames - 1]?.pipe}`,
+            `DATA: ${data[frames - 1]?.data}`,
+            '<<<',
+          );
+        }
+      },
+      function (isStopped: unknown, by_user: unknown, error_count: unknown) {
+        console.log('RADIO STOPPED! -> ', isStopped, by_user, error_count);
+      },
+    );
+
+    // const data: Buffer = Buffer.from('Hello mother fucker!');
+    // rf24.useWritePipe('0x72646f4e31');
+    // const go = () => {
+    //   rf24.write(data, function (success: unknown) {
+    //     console.log(`++ data sent! Success?: ${success}`);
+    //   });
+    // };
+    // let i = 0;
+    //
+    // const interval = setInterval(() => {
+    //   if (i <= 50) {
+    //     go();
+    //     i++;
+    //   } else {
+    //     clearInterval(interval);
+    //   }
+    // }, 1000);
   }
 
   public async start() {
