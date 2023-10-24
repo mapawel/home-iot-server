906b0898a70c739894b63b20bbf9e5a3a38a644f
mapawel <pawelma@gmail.com>
2023-10-24 21:02:09 +0200

    Message validation settings changed

--- radio-read-provider/src/radio-communication/entities/message.entity.ts
+++ radio-read-provider/src/radio-communication/entities/message.entity.ts
@@ -2,15 +2,15 @@ import { IsString, Length } from 'class-validator';
 
 class Message {
   @IsString()
-  @Length(1, 80)
+  @Length(10, 12)
   readonly moduleId: string;
 
   @IsString()
-  @Length(1, 80)
+  @Length(40, 300)
   readonly encryptedData: string;
 
   @IsString()
-  @Length(1, 80)
+  @Length(64, 64)
   readonly hash: string;
 }
 
